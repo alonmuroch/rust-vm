1d499c68636ecef6acd7857aa6ea9836aa023989
Alon Muroch <alonmuroch@gmail.com>
2025-06-22 12:12:32 +0300

    fix log

--- crates/program/src/log.rs
+++ crates/program/src/log.rs
@@ -3,7 +3,7 @@ macro_rules! logf_syscall {
     ($fmt_ptr:expr, $fmt_len:expr, $args_ptr:expr, $args_len:expr) => {{
         unsafe {
             core::arch::asm!(
-                "li a7, 4",  // syscall_storage_read
+                "li a7, 4",  // syscall_log
                 "ecall",
                 in("t0") $fmt_ptr,
                 in("t1") $fmt_len,
@@ -29,11 +29,20 @@ macro_rules! logf {
 
     ($fmt:expr, $($arg:expr),+ $(,)?) => {{
         const MAX_ARGS: usize = 32;
-        let mut args: [u32; MAX_ARGS] = [0; MAX_ARGS];
-        let mut j = 0;
+        let args_buf: [u32; MAX_ARGS] = {
+            let temp_args = [$($arg as u32),+];
+            let mut buffer = [0u32; MAX_ARGS];
+            let count = temp_args.len();
+            let mut i = 0;
+            while i < count {
+                buffer[i] = temp_args[i];
+                i += 1;
+            }
+            buffer
+        };
 
-        let mut arg_iter = [$($arg),+].into_iter();
         let fmt_bytes: &[u8] = $fmt;
+        let mut j = 0;
         let mut i = 0;
 
         while i < fmt_bytes.len() {
@@ -42,41 +51,12 @@ macro_rules! logf {
                 if i >= fmt_bytes.len() {
                     $crate::vm_panic(b"logf: format string ends with bare '%'");
                 }
-
                 match fmt_bytes[i] {
                     b'd' | b'u' | b'x' => {
-                        let val = arg_iter.next().unwrap_or_else(|| {
-                            $crate::vm_panic(b"logf: missing integer argument");
-                        });
-                        args[j] = val;
                         j += 1;
                     }
-                    // b'f' => {
-                    //     let val = arg_iter.next().unwrap_or_else(|| {
-                    //         $crate::vm_panic(b"logf: missing float argument");
-                    //     });
-                    //     args[j] = (val as f32).to_bits();
-                    //     j += 1;
-                    // }
-                    // b'c' => {
-                    //     let val = arg_iter.next().unwrap_or_else(|| {
-                    //         $crate::vm_panic(b"logf: missing char argument");
-                    //     });
-                    //     args[j] = val as u32;
-                    //     j += 1;
-                    // }
-                    // b'b' => {
-                    //     let ptr = arg_iter.next().unwrap_or_else(|| {
-                    //         $crate::vm_panic(b"logf: missing slice ptr");
-                    //     });
-                    //     let len = arg_iter.next().unwrap_or_else(|| {
-                    //         $crate::vm_panic(b"logf: missing slice len");
-                    //     });
-                    //     args[j] = ptr;
-                    //     args[j + 1] = len;
-                    //     j += 2;
-                    // }
-                    _ => $crate::vm_panic(b"logf: unknown format specifier"),
+                    b'%' => {} // escape
+                    _ => continue, // ignore unsupported for now
                 }
             }
             i += 1;
@@ -84,7 +64,7 @@ macro_rules! logf {
 
         let fmt_ptr = fmt_bytes.as_ptr();
         let fmt_len = fmt_bytes.len();
-        let args_ptr = args.as_ptr();
+        let args_ptr = args_buf.as_ptr();
         let args_len = j * core::mem::size_of::<u32>();
 
         $crate::logf_syscall!(fmt_ptr, fmt_len, args_ptr, args_len);
